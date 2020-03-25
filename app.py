2a7e971efe5446beabed1a84d407dd16acb0301a
Aaron <abrom010@fiu.edu>
2020-03-25 16:14:13 -0400

    added a jquery get request, modifies the html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import os
 application = flask.Flask(__name__)
 
  #WHEN URL IS http://nameofwebsite.com/ DO THIS
@@ -6,6 +7,11 @@ application = flask.Flask(__name__)
 def main():
     return flask.render_template('index.html')
 # ^^^ RETURNS index.html AFTER HAVING PROCESSED IT USING THE render_template() FUNCTION
+
+@application.route('/request',methods=['GET'])
+def request():
+    if flask.request.method == 'GET':
+        return 'COMMUNICATION SUCCESSFUL'
 
 #ONLY RUNS THE FLASK APPLICATION IF THE APP.PY IS BEING USED AS THE DRIVER
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,10 +1,13 @@
 
 //WINDOW.ONLOAD IS CALLED AFTER THE HTML FILE IS LOADED
 window.onload = function(){
-  var text = "This text was made using Javascript"
+  var text = "This text was made using Javascript. "
+  let url = '/request'
 
-  //THIS GETS THE ELEMENT OF THE DOCUMENT BY ID AND MODIFIES THE HTML INSIDE
-  document.getElementById("first_paragraph").innerHTML = text;
+//JQUERY MAKES A GET REQUEST TO THE GIVEN URL AND RUNS THAT FUNCTION
+  $.get(url,function(data,status){
+    //THIS GETS THE ELEMENT OF THE DOCUMENT BY ID AND MODIFIES THE HTML INSIDE
+       document.getElementById("first_paragraph").innerHTML = text+data;
+    })
+
 };
-
-//TO USE DATA FROM THE SERVER WE WOULD MAKE A REQUEST AND THEN USE THE ABOVE METHOD TO MODIFY THE HTML
