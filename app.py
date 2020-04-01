04e75697a9c7149fadd6628b1f532345bc0412b7
tchacko <tchacko@uncc.edu>
2020-03-31 20:23:27 -0400

    db routing start

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import flask
+from flask import jsonify
 import os
+import mysql.connector
+
 application = flask.Flask(__name__)
 
  #WHEN URL IS http://nameofwebsite.com/ DO THIS
@@ -8,11 +11,26 @@ def main():
     return flask.render_template('index.html')
 # ^^^ RETURNS index.html AFTER HAVING PROCESSED IT USING THE render_template() FUNCTION
 
+@application.route('/store',methods=['GET'])
+def store():
+    return flask.render_template('storepage.html')
+
+@application.route('/queue')
+def queue():
+    return flask.redirect('/store', code=302)
+
 @application.route('/request',methods=['GET'])
 def request():
     if flask.request.method == 'GET':
-        return 'COMMUNICATION SUCCESSFUL'
+        list = []
+        db = mysql.connector.connect(host="localhost", user="root", passwd="root", db="hackathon")
+        cur = db.cursor()
+        cur.execute("SELECT grocery_id FROM groceryStores WHERE zip_code='27560';")
+        for i in cur:
+            list.append(i)
+        return jsonify(list)
+
 
 #ONLY RUNS THE FLASK APPLICATION IF THE APP.PY IS BEING USED AS THE DRIVER
 if __name__ == '__main__':
-	application.run()
+    application.run()
--- static/main.js
+++ static/main.js
@@ -1,15 +1,21 @@
 
 //WINDOW.ONLOAD IS CALLED AFTER THE HTML FILE IS LOADED
+function storePage()
+{
+// console.log("Hey")
+  location.replace('/store')
+}
 window.onload = function(){
   let text = "This text was made using Javascript. "
   let url = '/request'
 
+  // console.log("1")
   function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
-    return xmlHttp.responseText;
+    return xmlHttp.response;
 }
 
 let data = httpGet(url)
