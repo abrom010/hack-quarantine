8aae462fb38aaee2d592767e692660af891f9361
Aaron <abrom010@fiu.edu>
2020-04-03 15:50:17 -0400

    cleaning

--- static/maps.js
+++ static/maps.js
@@ -5,16 +5,13 @@ var geocoder;
 var autocomplete;
 var markers = [];
 
-// const sleep = (milliseconds) => {
-//   return new Promise(resolve => setTimeout(resolve, milliseconds))
-// }
 
 function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.response;
-  }
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  return xmlHttp.response;
+}
 
 function initMap() {
   var latlong = new google.maps.LatLng(0, 0);
@@ -36,50 +33,38 @@ function initMap() {
 
   map.addListener('bounds_changed',search)
   map.addListener('zoom_changed',search)
-
 }
 
 function onPlaceChanged() {
-        console.log('place changed')
-        var place = autocomplete.getPlace();
-        if (place.geometry) {
-          map.panTo(place.geometry.location);
-          map.setZoom(10);
-          search();
-        } else {
-          document.getElementById('autocomplete').placeholder = 'Enter a city';
-        }
-      }
+  console.log('place changed')
+  var place = autocomplete.getPlace();
+  if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(10);
+    search();
+  } else {
+    document.getElementById('autocomplete').placeholder = 'Enter a city';
+  }
+}
 
 function search() {
-        bounds = map.getBounds();
-        zoom = map.getZoom();
-        
-        for(var i = 0; i < markers.length; i++){
-          marker = markers[i]
-          console.log(marker)
-          if(marker){
-            if(bounds.contains(marker.getPosition()) && zoom > 5){
-              marker.setMap(map)
-              console.log("map set!")
-            } else {
-              marker.setMap(null)
-              console.log("not work :(")
-            }
-          } else { console.log("not marker D:")};
-        }
-
-        
+  bounds = map.getBounds();
+  zoom = map.getZoom();
+  
+  for(var i = 0; i < markers.length; i++){
+    marker = markers[i]
+    console.log(marker)
+    if(marker){
+      if(bounds.contains(marker.getPosition()) && zoom > 5){
+        marker.setMap(map)
+        console.log("map set!")
+      } else {
+        marker.setMap(null)
+        console.log("not work :(")
       }
-
-// function clearMarkers() {
-//         for (var i = 0; i < markers.length; i++) {
-//           if (markers[i]) {
-//             markers[i].setMap(null);
-//           }
-//         }
-//         markers = [];
-//       }
+    } else { console.log("not marker D:")};
+  }
+}
 
 function addresses_to_markers() {
   let data = httpGet('/addresses')
@@ -89,7 +74,6 @@ function addresses_to_markers() {
   for(var i = 0; i < addresses.length; i++){
     var address = addresses[i]
     console.log(address)
-    // sleep(5000).then(() => {
       geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         let place = results[0]
@@ -97,8 +81,7 @@ function addresses_to_markers() {
       } else {
         alert('Geocode was not successful for the following reason: ' + status)
       }
-      });
-    // })
+    });
   }
 }
 
