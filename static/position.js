cb0fbbf26ef36572553993f894245cf768ba93fa
Aaron <abrom010@fiu.edu>
2020-04-11 21:58:36 -0400

    do do do

--- static/position.js
+++ static/position.js
@@ -11,16 +11,23 @@ window.onload = function() {
   // var xmlHttp = new XMLHttpRequest();
   let data = httpGet('/populateNames/'+num)
   names = JSON.parse(data)
-  for (var i = 1; i<names.length; i++) {
+  for (var i = 1; i<6; i++) {
     id = 'name' + i.toString();
-    document.getElementById(id).innerHTML = names[i - 1]
+    name = names[i-1]
+    if(name!="undefined"){
+      console.log("did")
+      document.getElementById(id).innerHTML = name
+    }
   }
 
   data = httpGet('/populateCodes/'+num)
   codes = JSON.parse(data)
-  for (var i = 1; i<codes.length; i++) {
+  for (var i = 1; i<6; i++) {
     id = 'code' + i.toString();
-    document.getElementById(id).innerHTML = codes[i - 1]
+    code = codes[i-1]
+    if(code){
+      document.getElementById(id).innerHTML = code
+    }
   }
   //console.log(fullData)
   //console.log(data)
